ddab1369b0744a926ac6ff21e96eba35800ec7e4
Dieter Käppel <dieter.kaeppel@gmail.com>
2019-04-13 00:08:22 +0200

    Don't fetch registry already on startup. This breaking server/client integration like zuul

--- eureka-client/src/main/java/com/netflix/discovery/DiscoveryClient.java
+++ eureka-client/src/main/java/com/netflix/discovery/DiscoveryClient.java
@@ -411,10 +411,6 @@ public class DiscoveryClient implements EurekaClient {
             throw new RuntimeException("Failed to initialize DiscoveryClient!", e);
         }
 
-        if (clientConfig.shouldFetchRegistry() && !fetchRegistry(false)) {
-            fetchRegistryFromBackup();
-        }
-
         // call and execute the pre registration handler before all background tasks (inc registration) is started
         if (this.preRegistrationHandler != null) {
             this.preRegistrationHandler.beforeRegistration();
